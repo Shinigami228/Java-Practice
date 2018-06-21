a2d2733f27948b8ad202f3fe25c44b050020f2d8
Shinigami228 <priyankaojha228@gmail.com>
2018-06-21 21:54:37 +0530

    Code for sum of n natural numbers

---
+++ src/Practice.java
@@ -0,0 +1,13 @@
+/**
+ * Created by TOSHHIBA on 6/21/2018.
+ */
+public class Practice {
+    public static void main(String[] args){
+        int n=5;
+        int value=0;
+        for(int i=0; i<=n; i++){
+           value=value+i;
+        }
+        System.out.println(value);
+    }
+}
